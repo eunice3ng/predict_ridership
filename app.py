1d717d4a9026b70a70b21fcadb39c6e1089e64d7
Eunice Eng <euniceeng04@gmail.com>
2025-06-05 14:36:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,13 @@ This app predicts **hourly ridership** at a selected KTM Komuter station based o
 is_weekend = st.selectbox("Is it a weekend?", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
 is_peak_hour = st.selectbox("Is it a peak hour?", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
 is_holiday = st.selectbox("Is it a public holiday?", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
-population = st.number_input("Population nearby", min_value=0, step=100)
+origin_encoded = st.number_input("Origin Station Code (encoded)", min_value=0, max_value=100, value=13)
+destination_encoded = st.number_input("Destination Station Code (encoded)", min_value=0, max_value=100, value=7)
 
 # Prediction button
 if st.button("Predict Ridership"):
     # Prepare the feature vector
-    input_data = np.array([[is_weekend, is_peak_hour, is_holiday, population]])
+    input_data = np.array([[is_weekend, is_peak_hour, is_holiday, origin_encoded,destination_encoded]])
     
     # Scale the input
     input_scaled = scaler.transform(input_data)
